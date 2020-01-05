d1c2f922f622eed56aa2aaf0a60558345b8cab6c
kymry burwell <kymry.burwell@gmail.com>
2020-01-04 16:55:59 -0800

    resctructured project

--- initialize_parameters.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-def initialize_w_b(num_nodes, num_variables):
-    """ initializes the weights or biases for one layer
-     with random numbers in [0.0, 1.0) """
-    np.random.seed(19920)
-    return np.random.rand(num_nodes, num_variables)
